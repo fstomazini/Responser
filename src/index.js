2cb354ae3667f59616580b84ce95eb31d62ecb72
fstomazini <felipe.s.tomazini@gmail.com>
2023-09-29 15:40:33 -0300

    cortes nas funcões não usadas

--- src/index.js
+++ src/index.js
@@ -18,27 +18,6 @@ const router = express.Router();
 const extratoController = require('./controllers/extrato.controller');
 const dashboardController = require('./controllers/dashboard.controller');
 
-//rotina para gerar balance
-router.get('/api/billing/generate-balance', extratoController.generateBalance);
-
-//retornar o saldo total
-router.get('/api/billing/balance', extratoController.saldo);
-
-//debitar o saldo
-router.post('/api/billing/remove-balance', extratoController.removeBalance);
-
-//debitar o saldo a partir da API (sem o bearer token da plataforma)
-router.post('/api/billing/remove-balance-api', extratoController.removeBalanceApi);
-
-//estornar o saldo ( telefones e emails )
-router.post('/api/billing/return-balance', extratoController.returnBalance);
-
-router.post('/api/billing/dados-dashboard', dashboardController.returnDadosDashboard);
-
-router.post('/api/billing/dados-tabelas-dashboard', dashboardController.returnDadosTabelasDashboard);
-
-router.post('/api/billing/top-filtros-dashboard', dashboardController.returnTopFiltrosDashboard);
-
 app.get('/health', health.HealthEndpoint(healthChecker));
 
 app.get('/verificar', dashboardController.procurador);
