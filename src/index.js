c4d3c759f197834264ba3b5c0bc5642d662376fd
fstomazini <felipe.s.tomazini@gmail.com>
2023-09-29 15:15:22 -0300

    mudanca porta

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.get('/health', health.HealthEndpoint(healthChecker));
 app.get('/verificar', dashboardController.procurador);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.set('port', port);
 
 const server = http.createServer(app);
