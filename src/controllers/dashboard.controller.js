bff66ed36bea83098dbd149aa522e10c4246ffa6
fstomazini <felipe.s.tomazini@gmail.com>
2023-09-29 18:54:17 -0300

    ajuste na resposta

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -18,8 +18,7 @@ const procurador = async(req , res) =>{
   
   let ipCliente = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
   
-
-  return res.status(200).json({httpResponse : "ok", htppStatus : 200 , ipCliente : ipCliente })
+  res.status(200).json({httpResponse : "ok", htppStatus : 200 , ipCliente : ipCliente })
 }
 
 async function getDadosTabelasDashboard(usuarios, cd_cliente) {
